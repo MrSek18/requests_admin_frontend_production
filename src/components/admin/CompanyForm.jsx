fe521878c758609d3989b96accc549e6fe006238
kevin <mrsek2022@gmail.com>
2025-12-20 23:45:51 -0500

    providerForm  modified

--- src/components/admin/CompanyForm.jsx
+++ src/components/admin/CompanyForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import api from '../../api';
-import { warmUpDatabase } from "./utils/warmUp";
+import { warmUpDatabase } from "../../utils/warmUp";
 
 export default function CompanyForm() {
   const [name, setName] = useState('');
--- src/components/admin/CompanyList.jsx
+++ src/components/admin/CompanyList.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import api from '../../api';
-import { warmUpDatabase } from "./utils/warmUp";
+import { warmUpDatabase } from "../../utils/warmUp";
 
 export default function CompanyList() {
   const [companies, setCompanies] = useState([]);
